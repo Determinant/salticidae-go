b0956649924d83f6b0cbb95ed34158d4d045bdde
Determinant <ted.sybil@gmail.com>
2019-12-06 10:45:17 -0500

    update go submodule recursively

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,13 +7,15 @@ source "${SRC_DIR}/env.sh"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     go get -u -d "github.com/$SALTICIDAE_ORG/salticidae-go"
+    cd "$SALTICIDAE_GO_PATH"
+    git submodule update --init --recursive
     cd "$SALTICIDAE_PATH"
     cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$SALTICIDAE_PATH/build" .
     make -j4
     make install
     cd -
     rm -f "$prefix/libsalticidae.a"
-    ln -sv "$GOPATH/src/github.com/$SALTICIDAE_ORG/salticidae-go/salticidae/libsalticidae.a" "$prefix/libsalticidae.a"
+    ln -sv "$SALTICIDAE_PATH/libsalticidae.a" "$prefix/libsalticidae.a"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install Determinant/salticidae/salticidae
 else
