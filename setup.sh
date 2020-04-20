a196ba8403df335a4e2d0e3f34de31b553eb099d
Alex Willmer <alex@moreati.org.uk>
2020-04-20 23:48:20 +0100

    build: Don't silence errors when bootstrapping build scripts

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ set -o pipefail
 
 tmpdir=$(mktemp -d -t salticidae-go-XXXXXXXX)
 cd "$tmpdir"
-curl -s https://raw.githubusercontent.com/ava-labs/salticidae-go/master/scripts/build.sh -o ./build.sh
-curl -s https://raw.githubusercontent.com/ava-labs/salticidae-go/master/scripts/env.sh -o ./env.sh
+curl -sS https://raw.githubusercontent.com/ava-labs/salticidae-go/master/scripts/build.sh -o ./build.sh
+curl -sS https://raw.githubusercontent.com/ava-labs/salticidae-go/master/scripts/env.sh -o ./env.sh
 chmod +x ./build.sh
 source ./env.sh
 ./build.sh
