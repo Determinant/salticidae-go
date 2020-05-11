dedceeb9552e67ffa46c3d2e0f3c00cad186f52a
Dan Laine <daniel.laine@avalabs.org>
2020-05-11 16:09:23 -0400

    update build process. Use go modules. Pull salticidae using go modules rather than using submodule

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ava-labs/salticidae-go
+
+go 1.14
+
+require github.com/ava-labs/salticidae v0.3.1 // Manually included so salticidae is pulled
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,16 +1,16 @@
-export GOPATH="$(go env GOPATH)"
-export SALTICIDAE_ORG="ava-labs"
-export SALTICIDAE_GO_VER="v0.1.0"
-export SALTICIDAE_GO_PATH="$GOPATH/src/github.com/$SALTICIDAE_ORG/salticidae-go"
+#!/bin/bash -e
+
+export SALTICIDAE_VER="v0.3.1"
+export SALTICIDAE_PATH=$GOPATH/pkg/mod/github.com/ava-labs/salticidae@$SALTICIDAE_VER
+export SALTICIDAE_GO_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd ) # Directory above this script
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    export SALTICIDAE_PATH="$SALTICIDAE_GO_PATH/salticidae"
     export CGO_CFLAGS="-I$SALTICIDAE_PATH/build/include/"
     export CGO_LDFLAGS="-L$SALTICIDAE_PATH/build/lib/ -lsalticidae -luv -lssl -lcrypto -lstdc++ -g"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     export CGO_CFLAGS="-I/usr/local/opt/openssl/include"
     export CGO_LDFLAGS="-L/usr/local/opt/openssl/lib/ -lsalticidae -luv -lssl -lcrypto"
 else
-    echo "Your system is not supported yet."
+    echo "Your operating system is not supported"
     exit 1
 fi
